1ae31371bdfd30e92421a74d5b76172ae8f79063
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-10 13:39:18 +0300

    update code

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,2 +1,23 @@
 module MyEnumerable
+    def all?(&block)
+        pass = true
+        each do |e|
+            pass = false if !block.call(e)
+        end
+        pass
+    end
+    def any?(&block)
+        pass =false
+        each do |e|
+            pass = true if block.call(e)
+        end
+        pass
+    end
+    def filter?(&block)
+        pass = []
+        each do |e|
+            pass.push(e) if block.call(e) 
+        end 
+        pass
+    end
 end
