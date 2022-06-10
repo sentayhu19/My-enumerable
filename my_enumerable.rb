cd02608ac82677ddc331161db806b6839cb6a4be
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-10 13:44:02 +0300

    fix error

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,23 +1,25 @@
 module MyEnumerable
-    def all?(&block)
-        pass = true
-        each do |e|
-            pass = false if !block.call(e)
-        end
-        pass
+  def all?(&block)
+    pass = true
+    each do |e|
+      pass = false if !block.call(e)
     end
-    def any?(&block)
-        pass =false
-        each do |e|
-            pass = true if block.call(e)
-        end
-        pass
+    pass
+  end
+
+  def any?(&block)
+    pass = false
+    each do |e|
+      pass = true if block.call(e)
     end
-    def filter?(&block)
-        pass = []
-        each do |e|
-            pass.push(e) if block.call(e) 
-        end 
-        pass
+    pass
+  end
+
+  def filter?(&block)
+    pass = []
+    each do |e|
+      pass.push(e) if block.call(e)
     end
+    pass
+  end
 end
